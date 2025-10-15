70c0fe0db7d4099a9d04b97f2aebe36892cc9d51
butter237 <66070237@kmitl.ac.th>
2025-10-15 11:08:26 +0700

    finary blog-detail

--- brogblog/blogs/urls.py
+++ brogblog/blogs/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     path('blog-bookmark/<int:blog_id>/', views.BlogBookmarkToggleView.as_view(), name='blog-bookmark'),
     path("report/<int:blog_id>/", views.ReportBlogView.as_view(), name="report_blog"),
     path("delete-blog/<int:blog_id>/", views.DeleteBlogView.as_view(), name="delete-blog"),
+    path("edit-blog/<int:blog_id>/", views.EditBlogView.as_view(), name="edit-blog"),
     path('category-detail/<str:name>/', views.CategoryDetailView.as_view(), name='category-detail'),
+    # path('tag-detail/<int:tag_id>/', views.TagDetailView.as_view(), name='tag-detail'),
     path('category-detail/<str:name>/<int:tag_id>/',  views.CategoryDetailView.as_view(), name='category-detail-tag'),
 ]
